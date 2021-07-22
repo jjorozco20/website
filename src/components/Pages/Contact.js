409db55311bc1a1a933dcee46f249a498690d08b
jjorozco20 <jorozco10@ucol.mx>
2021-07-22 16:20:05 -0500

    Contact.js form validation with Yup

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Field from '../Common/Field';
 import { withFormik } from 'formik';
+import * as Yup from 'yup';
 
 const fields = {
     sections: [
@@ -16,64 +17,55 @@ const fields = {
     ]
 }
 
-class Contact extends Component{
+function Contact (props){
 
-    submitForm = (e) =>{
-        alert("Form submitted, thank you very much!");
-    }
-
-    
-
-    render (){
-        return (
-            <section id="contact">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 text-center">
-                            <h2 className="section-heading">Contact Us</h2>
-                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <form name="sentMessage" noValidate onSubmit={e=> this.props.handleSubmit}>
-                                <div className="row">
-                                    {fields.sections.map((section, sectionIndex) => {
-                                        console.log('Rendering section', sectionIndex, "with", section);
-                                        return (
-                                            <div className="col-md-6" key={sectionIndex}>
-                                                {section.map((field, index) => {
-                                                    return <Field 
-                                                    {...field} 
-                                                    key = {index} 
-                                                    value = {this.props.values[field.name]}
-                                                    name = {field.name}
-                                                    onChange = {this.props.handleChange}
-                                                    onBlur = {this.props.handleBlur}
-                                                    touched = {(this.props.touched[field.name])}
-                                                    errors = {this.props.errors[field.name]} />
-                                                })}
-                                            </div>
-                                        )
-                                    })}
-                                    <div className="clearfix"></div>errors
-                                    <div className="col-lg-12 text-center">
-                                        <div id="success"></div>
-                                        <button 
-                                          id="sendMessageButton" 
-                                          type="submit" 
-                                          className="btn btn-primary btn-xl text-uppercase"
-                                          >Send Message
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+    return (
+        <section id="contact">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <h2 className="section-heading">Contact Us</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     </div>
                 </div>
-            </section>
-        );
-    }
+                <div className="row">
+                    <div className="col-lg-12">
+                        <form name="sentMessage" noValidate onSubmit={e=> this.props.handleSubmit}>
+                            <div className="row">
+                                {fields.sections.map((section, sectionIndex) => {
+                                    console.log('Rendering section', sectionIndex, "with", section);
+                                    return (
+                                        <div className="col-md-6" key={sectionIndex}>
+                                            {section.map((field, index) => {
+                                                return <Field 
+                                                {...field} 
+                                                key = {index} 
+                                                value = {props.values[field.name]}
+                                                name = {field.name}
+                                                onChange = {props.handleChange}
+                                                onBlur = {props.handleBlur}
+                                                touched = {(props.touched[field.name])}
+                                                errors = {props.errors[field.name]} />
+                                            })}
+                                        </div>
+                                    )
+                                })}
+                                <div className="clearfix"></div>errors
+                                <div className="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button
+                                        type="submit" 
+                                        className="btn btn-primary btn-xl text-uppercase"
+                                        >Send Message
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 export default withFormik({
     mapPropsToValues: () =>({
@@ -82,17 +74,25 @@ export default withFormik({
         phone: '',
         message: '',
     }),
-    validate: values => {
-       const errors = {};
-       Object.keys(values).map(v => {
-           if(!values[v]){
-               errors[v] = "Required"
-           }
-       })
-       return errors; 
-    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().min(3, 'Come on, bud, your name is longer than that.').required('You must give us your name.'),
+        email: Yup.string().email('You need to give us a valid email.').required('We need your email.'),
+        phone: Yup.string()
+        	.min(10, 'Please provide your 10 digit phone number.')
+					.max(15, 'Your phone number is too long.')
+        	.required('We need a phone number to reach you at.'),
+        message: Yup.string()
+					.min(30, 'You need to provide us more details.')
+					.max(300, 'Enough explanation.')
+					.required('Message is required.')
+    }),
     handleSubmit: (values, {setSubmitting}) => {
         console.log("VALUES", values);
         alert("You have submitted the form", JSON.stringify(values));
+				if (!values){
+					setSubmitting(false);
+				}else{
+					setSubmitting(true);
+				}
     }
 })(Contact);
